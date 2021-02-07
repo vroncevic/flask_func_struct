77ca315924f8715ecd5f53f9582480ad939c3730
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-02-07 17:31:49 +0100

    [flask_func_struct] Updated processing error on import, docs

--- manage.py
+++ manage.py
@@ -1,37 +1,65 @@
 # -*- coding: utf-8 -*-
+
+"""
+ Module
+     manage.py
+ Copyright
+     Copyright (C) 2017 Vladimir Roncevic <elektron.ronca@gmail.com>
+     flask_func_struct is free software: you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+     flask_func_struct is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+     See the GNU General Public License for more details.
+     You should have received a copy of the GNU General Public License along
+     with this program. If not, see <http://www.gnu.org/licenses/>.
+ Info
+     Define manage routines for Flask App.
+     Main entry point.
+"""
+
+import sys
+
+try:
+    import coverage
+    from flask_migrate import Migrate, MigrateCommand
+    from flask_script import Manager
+    from manage_commands.create_database import CreateDatabase
+    from manage_commands.drop_database import DropDatabase
+    from manage_commands.create_data import CreateData
+    from manage_commands.create_superuser import CreateSuperUser
+    from manage_commands.run_coverage import RunCoverage
+    from manage_commands.run_test import RunTest
+    from manage_commands.orm_test import ORMTest
+
+    COV = coverage.coverage(
+        branch=True,
+        include="app_server/*",
+        omit=[
+            "app_server/tests/*",
+            "app_server/configuration/test_config.py",
+            "app_server/*/__init__.py"
+        ]
+    )
+
+    COV.start()
+
+    from app_server import app, db
+
+except ImportError as error_message:
+    MESSAGE = "\n{0}\n{1}\n".format(__file__, error_message)
+    sys.exit(MESSAGE)  # Force close python ATS ##############################
+
 __author__ = "Vladimir Roncevic"
 __copyright__ = "Copyright 2017, Free software to use and distributed it."
 __credits__ = ["Vladimir Roncevic"]
 __license__ = "GNU General Public License (GPL)"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __maintainer__ = "Vladimir Roncevic"
 __email__ = "elektron.ronca@gmail.com"
 __status__ = "Updated"
-
-import coverage
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from manage_commands.create_database import CreateDatabase
-from manage_commands.drop_database import DropDatabase
-from manage_commands.create_data import CreateData
-from manage_commands.create_superuser import CreateSuperUser
-from manage_commands.run_coverage import RunCoverage
-from manage_commands.run_test import RunTest
-from manage_commands.orm_test import ORMTest
-
-COV = coverage.coverage(
-    branch=True,
-    include="app_server/*",
-    omit=[
-        "app_server/tests/*",
-        "app_server/configuration/test_config.py",
-        "app_server/*/__init__.py"
-    ]
-)
-
-COV.start()
-
-from app_server import app, db
 
 migrate = Migrate(app, db)
 manager = Manager(app)
