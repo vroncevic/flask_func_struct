825213007693c9e3e0a5b5006eeff0782fcfc947
Vladimir Roncevic <elektron.ronca@gmail.com>
2021-06-16 19:56:58 +0200

    [flask_func_struct] updated view modules, apply rule 79

--- app_server/views/user/__init__.py
+++ app_server/views/user/__init__.py
@@ -5,8 +5,8 @@
      __init__.py
  Copyright
      Copyright (C) 2017 Vladimir Roncevic <elektron.ronca@gmail.com>
-     flask_func_struct is free software: you can redistribute it and/or modify it
-     under the terms of the GNU General Public License as published by the
+     flask_func_struct is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by the
      Free Software Foundation, either version 3 of the License, or
      (at your option) any later version.
      flask_func_struct is distributed in the hope that it will be useful, but
